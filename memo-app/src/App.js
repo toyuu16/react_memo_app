60513f223521175a2b52258312105f8c3bde5f18
toyuu16 <ssta0ut0@gmail.com>
2021-10-10 11:04:49 +0900

    修正コード

--- memo-app/src/App.js
+++ memo-app/src/App.js
@@ -1,5 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import Board from './Board';
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/users">Users</Link>
+
+      <Switch>
+        <Route path="/about">
+          <h1>About</h1>
+        </Route>
+        <Route path="/users">
+          <h1>Users</h1>
+        </Route>
+        <Route path="/">
+          <h1>Home</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 test('renders learn react link', () => {
   // render(<Board />);
